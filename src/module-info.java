9c06c817ceddd5fcc55a8650bd7108528052f131
Arièle <ariele.ratsimbazafy@gmail.com>
2025-03-17 22:22:50 +0400

    First commit

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+/*
+ * module WindowsBuilderTest { requires java.desktop; requires JTattoo; //
+ * requires jdk.incubator.vector; requires junit; requires java.sql; }
+ */
